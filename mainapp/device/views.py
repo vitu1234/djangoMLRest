76f19227441459eeeed24d6318b07b6a30c97f06
Vitu Mafeni <mafeniblessed@gmail.com>
2023-02-03 00:23:20 +0900

    reworked add user device

--- mainapp/device/views.py
+++ mainapp/device/views.py
@@ -62,21 +62,71 @@ def add_user_device(request):
         return Response({"error":True, "msg":"Farm name not set"}, status=status.HTTP_400_BAD_REQUEST)
     if(request_data_copy.get('device_id') is None):
         return Response({"error":True, "msg":"Device ID not set"}, status=status.HTTP_400_BAD_REQUEST)
+    if(request_data_copy.get('device_type') is None):
+        return Response({"error":True, "msg":"Device type not set"}, status=status.HTTP_400_BAD_REQUEST)
     if(request_data_copy.get('device_name') is None):
         return Response({"error":True, "msg":"Device Name not set"}, status=status.HTTP_400_BAD_REQUEST)
     
-    serializer = UserDeviceSerializer(data=request_data_copy)
-    if serializer.is_valid():
-        serializer.save()
-        #update the unclaimed devices records
-        database = ApiConfig.get_mongo_database()
-        collection = database["devices"]
-        myquery = { "flotta_egdedevice_id": request_data_copy.get('device_id') }
-        newvalues = { "$set": {"user_claim":True} }
-        collection.update_one(myquery, newvalues)
+    if request_data_copy.get('device_type') == 'sensor':
+        if(request_data_copy.get('raw_readings_type') is None):
+            return Response({"error":True, "msg":"Device readings type not set"}, status=status.HTTP_400_BAD_REQUEST)
+        if(request_data_copy.get('raw_readings_units_type') is None):
+            return Response({"error":True, "msg":"Device readings units type not set"}, status=status.HTTP_400_BAD_REQUEST)
+     #check if device already exists
+     #    
+    database = ApiConfig.get_mongo_database()
+    collection1 = database["user_devices"]
+    cursor1 = collection1.find({ "device_id": request_data_copy.get('device_id') })
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if(cursor1.count()==0):
+        serializer = UserDeviceSerializer(data=request_data_copy)
+        if serializer.is_valid():
+            serializer.save()
+            #update the unclaimed devices records and set device type 
+            #sensor has readings while actuators does not have readings
+            
+            collection = database["devices"]
+            myquery = { "flotta_egdedevice_id": request_data_copy.get('device_id') }
+            cursor = collection.find(myquery)
+            if request_data_copy.get('device_type') == 'sensor':
+                if cursor.count() > 0:
+                    newvalues = { 
+                    "$set": {
+                            'user_claim':True,
+                            'device_type': request_data_copy.get('device_type'),
+                            'columns_readings_type':((request_data_copy.get('raw_readings_type').lower()).strip()).replace(" ", ""),
+                            'columns_readings_units_type':(request_data_copy.get('raw_readings_units_type').strip()).replace(" ", "")
+                            }
+                    }
+                    collection.update_one(myquery, newvalues)
+                else:
+                    newdevice = {
+                            'user_claim':True,
+                            'device_type': request_data_copy.get('device_type'),
+                            'columns_readings_type':((request_data_copy.get('raw_readings_type').lower()).strip()).replace(" ", ""),
+                            'columns_readings_units_type':(request_data_copy.get('raw_readings_units_type').strip()).replace(" ", "")
+                            }
+                    collection.insert_one(newdevice)
+            else:
+                if cursor.count() > 0:
+                    newvalues = { 
+                    "$set": {
+                            'user_claim':True,
+                            'device_type': request_data_copy.get('device_type')
+                            }
+                    }
+                    collection.update_one(myquery, newvalues)
+                else:
+                    newdevice = {
+                            'user_claim':True,
+                            'device_type': request_data_copy.get('device_type')
+                            }
+                    collection.insert_one(newdevice)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"error": True, "msg":"Device has already been added"}, status=status.HTTP_400_BAD_REQUEST)
 
 #get user devices
 @api_view(['GET'])
@@ -104,8 +154,11 @@ def user_devices(request):
                 for document2 in cursor2:
                     
                     collection3 = database["sensors"]
-                    cursor3 = collection3.find_one({"flotta_egdedevice_id":document2['device_id']},sort=[( 'timestamp', pymongo.DESCENDING )]) 
-                    
+                    cursor3 = collection3.find_one({"flotta_egdedevice_id":document2['device_id']},sort=[( 'timestamp', pymongo.DESCENDING )])
+
+                    collection4 = database["devices"]
+                    cursor4 = collection4.find_one({"flotta_egdedevice_id":document2['device_id']})
+
                     date_time_str = str(cursor3['timestamp'])
                     date_time_obj =datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
                     date_time_str = date_time_obj.strftime('%Y-%m-%d %H:%M:%S')
@@ -117,9 +170,6 @@ def user_devices(request):
                         "device_name": document2['device_name'],
                         "switch_status": document2['switch_status'],
                         "description": document2['description'],
-                        "temperature":  cursor3['temperature'],
-                        "humidity":  cursor3['humidity'],
-                         "soil_moisture":  cursor3['soil_moisture'],
                          "timestamp": date_time_str,
 
                         "user_id": document['user_id'],
@@ -128,6 +178,19 @@ def user_devices(request):
                         "longtude": document['longtude'],
                         "latitude": document['latitude'],
                     }
+
+                    raw_readings_type_list =  (((cursor4['columns_readings_type'].lower()).strip()).replace(" ", "")).split(",") #lower case,remove commas and spaces and covert to array
+                    raw_readings_units_type_list =  (((cursor4['columns_readings_units_type'].lower()).strip()).replace(" ", "")).split(",") #lower case,remove commas and spaces and covert to array
+                    i = 0
+                    units={}
+                    for value in raw_readings_type_list:
+                        if value !="":
+                            row[value]=cursor3[value]
+                            if i <= len(raw_readings_units_type_list):
+                                units[value]=raw_readings_units_type_list[i]
+                        i+=1        
+                            
+                    row['units']=units
                     user_devices.append(row)
         data_array = {
             "error":False, 
